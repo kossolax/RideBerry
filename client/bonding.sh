bb7bf7c8af87b447b0685d42a2418ebf82b7a674
Steve ZARETTI <steve@zaretti.be>
2024-06-15 10:08:39 +0200

    added basic client

---
+++ client/bonding.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+keypath="/root/gt.key"
+
+# check if we are root
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root"
+    exit 1
+fi
+
+# check if glorytun is installed
+if [ ! -f "/usr/sbin/glorytun" ]; then
+    echo "glorytun is not installed, attempt to install it"
+    wget https://github.com/angt/glorytun/releases/download/v0.3.4/glorytun-0.3.4-$(arch)-linux-musl.bin -O /usr/sbin/glorytun
+    chmod +x /usr/sbin/glorytun
+fi
+
+# check if we have a keyfile
+if [ ! -f "$keypath" ]; then
+    echo "keyfile not found! import it from server and place it at $keypath"
+	exit 1
+fi
+
+echo "setup DNS"
+echo "nameserver 1.1.1.1" > /etc/resolv.conf
+echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+
+echo "getting connexion info"
+server=$(dig +short vps.zaretti.be | tail -n 1)
+wlan=$(ip addr show wlan0 | awk '/inet / {print $2}' | xargs ipcalc -n | awk '/Network:/ {print $2}')
+
+client0=$(ifconfig wwan0 | awk '/inet / {print $2}')
+network0=$(ip addr show wwan0 | awk '/inet / {print $2}' | xargs ipcalc -n | awk '/Network:/ {print $2}')
+gateway0=$(ip route show dev wwan0 | awk '/default via/ {print $3}')
+
+client1=$(ifconfig wwan1 | awk '/inet / {print $2}')
+network1=$(ip addr show wwan1 | awk '/inet / {print $2}' | xargs ipcalc -n | awk '/Network:/ {print $2}')
+gateway1=$(ip route show dev wwan1 | awk '/default via/ {print $3}')
+
+echo "starting vpn to $server"
+glorytun bind 0.0.0.0 to $server keyfile $keypath &
+sleep 3
+
+echo "adding $client to bound"
+ifconfig tun0 10.0.1.2 pointopoint 10.0.1.1 up
+glorytun path up $client0 rate tx 10mbit rx 10mbit
+glorytun path up $client1 rate tx 10mbit rx 10mbit
+
+echo "changing route"
+
+echo "1" > /proc/sys/net/ipv4/ip_forward
+if ! grep -q wwan0 /etc/iproute2/rt_tables; then
+	echo "100	wwan0" >> /etc/iproute2/rt_tables
+fi
+if ! grep -q wwan1 /etc/iproute2/rt_tables; then
+	echo "200	wwan1" >> /etc/iproute2/rt_tables
+fi
+
+ip route add $network0 dev wwan0 src $client0 table 100
+ip route add default via $gateway0 table 100
+
+ip route add $network1 dev wwan1 src $client1 table 200
+ip route add default via $gateway1 table 200
+
+ip route del default dev wwan0
+ip route del default dev wwan1
+ip route add default via 10.0.1.2 dev tun0
+
+ip rule add from $client0 table 100
+ip rule add from $client1 table 200
+
+ip route add $wlan dev wlan0 table 100
+ip route add $network1 dev wwan1 table 100
+ip route add 127.0.0.0/8 dev lo table 100
+
+ip route add $wlan dev wlan0 table 200
+ip route add $network0 dev wwan0 table 200
+ip route add 127.0.0.0/8 dev lo table 200
+
+echo "ready"
+sleep inf
---
+++ client/modem.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+apn=$1
+wwan=$2
+
+# check if we are root
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root"
+  exit 1
+fi
+# check if env vars are set
+if [ -z "$wwan" ]; then
+  echo "wwan is not set"
+  exit 1
+fi
+if [ -z "$apn" ]; then
+  echo "apn is not set"
+  exit 1
+fi
+if [ ! -f "/usr/sbin/simcom-cm" ]; then
+  echo "simcom-cm is not installed"
+  exit 1
+fi
+if [ ! -f "/usr/bin/screen" ]; then
+  echo "screen is not installed"
+  exit 1
+fi
+
+
+is_interface_valid() {
+  if ip addr show $wwan 2>/dev/null | grep -qe "state UP" -e "inet " ; then
+    return 0
+  fi
+  return 1
+}
+is_screen_valid() {
+  if /usr/bin/screen -list | grep -q $wwan; then
+    return 0
+  fi
+  return 1
+}
+start() {
+  echo -n "starting..."
+
+  /usr/bin/screen -L -Logfile $pipe -dmS $wwan /usr/sbin/simcom-cm -s $apn -i $wwan
+  while ! is_interface_valid; do
+    sleep 1
+  done
+  sleep 1
+
+  echo "complete"
+}
+stop() {
+  echo -n "stopping..."
+
+  /usr/bin/screen -S $wwan -X quit
+  ifconfig $wwan down
+  sleep 1
+  
+  echo "complete"
+}
+restart() {
+  stop
+  start
+}
+
+
+
+last_log=""
+pipe="/tmp/screen_$wwan.pipe"
+rm -f $pipe 2>/dev/null
+touch $pipe
+if is_screen_valid; then
+  /usr/bin/screen -S $wwan -X hardcopy $pipe
+else
+  start
+fi
+
+(
+  while true; do
+
+    log=$(tail -n 5 $pipe | grep "\S" | tail -n 1)
+    if [ "$last_log" != "$log" ]; then
+      last_log=$log
+      echo $last_log
+    fi
+
+    # Restart the connection if the screen session is not active
+    if ! is_screen_valid; then
+      echo "No active screen session found for $wwan. Restarting..."
+      start
+    fi
+
+    # Restart the connection if the interface is down
+    if ! is_interface_valid; then
+      echo "Interface $wwan is down. Restarting..."
+      restart
+    fi
+
+    # Restart the connection if the log indicates it was lost
+    if [[ "$last_log" == *"DataCap: UNKNOW"* ]]; then
+      echo "Connection lost. Restarting..."
+      restart
+    fi
+
+    sleep 1
+  done
+) &
+
+echo "Monitoring started for $wwan..."
